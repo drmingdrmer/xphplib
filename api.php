7d010b6d70a5d4d96056d4743ef6e5238110b3f9
drdr xp <drdr.xp@gmail.com>
2014-07-10 01:53:48 +0800

    add api.php under dev

---
+++ api.php
@@ -0,0 +1,24 @@
+<? 
+
+
+class XPL_Response {
+    function __construct( $code, $data=NULL ) {
+        $this->code = $code;
+        $this->data = $data;
+    }
+
+    function output( $format ) {
+        // TODO return code
+        
+    }
+}
+
+function xpl_resp( $d, $fmt ) {
+    
+}
+
+
+?>
+
+
+
